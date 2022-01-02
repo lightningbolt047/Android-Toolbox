d5a8c90556e7b35ca7c83eefc845fa0ce0dcf9f0
Sashank <sashankvisweshwaran@gmail.com>
2022-01-02 17:11:50 +0530

    Code cleanup and added groundwork for linux support

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:adb_gui/screens/connection_initiation_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,7 +14,7 @@ import 'package:adb_gui/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ConnectionInitiationScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
